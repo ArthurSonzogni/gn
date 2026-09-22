9ed7ee32c3f57cddc200f266d5c410fef037563e
Matt Stark <msta@google.com>
2026-09-22 15:43:39 +1000

    Create an empty integration test for starlark
    The toolchain configuration is copied from integration_tests/simple.
    
    Change-Id: I9b60d018a93bd915e5ea5495aa3814546a6a6964
    Reviewed-on: https://gn-review.googlesource.com/c/gn/+/26780
    Reviewed-by: Takuto Ikuta <tikuta@google.com>

--- build/gen.py
+++ build/gen.py
@@ -475,7 +475,10 @@ def WriteGenericNinja(path, static_libraries, executables,
               args='--quiet',
           ),
           ninja.Phony(
-              'run_integration_tests', inputs=[ninja.IntegrationTest('simple')]
+              'run_integration_tests',
+              inputs=[ninja.IntegrationTest('simple')]
+              + ([ninja.IntegrationTest('starlark')]
+                 if options.starlark else []),
           ),
       ] + ([
           ninja.RunBinary(
